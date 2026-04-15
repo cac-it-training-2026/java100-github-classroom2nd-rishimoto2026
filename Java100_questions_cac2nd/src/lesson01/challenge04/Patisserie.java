70e0822b5c29f99492b07466ef8b823fc1aa9add
rishimo <rishimo@C010422941.zipangu.cac.co.jp>
2026-04-15 10:28:11 +0900

    lesson1完了

--- Java100_questions_cac2nd/src/lesson01/challenge04/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge04/Patisserie.java
@@ -28,6 +28,38 @@
 
 package lesson01.challenge04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+		int cit = 30;
+		int cho = 30;
+		int pis = 30;
+
+		System.out.println("本日のおすすめ商品です。\n");
+		System.out.println("シトロン\t\t\\250・・・残り" + cit + "個");
+		System.out.println("ショコラ\t\t\\280・・・残り" + cho + "個");
+		System.out.println("ピスターシュ\t\\320・・・残り" + pis + "個");
+
+		System.out.println("それぞれ何個ずつ買いますか？（最大30個まで）\n");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン\t\t>");
+		String citnum = reader.readLine();
+		System.out.print("ショコラ\t\t>");
+		String chonum = reader.readLine();
+		System.out.print("ピスターシュ\t>");
+		String pisnum = reader.readLine();
+
+		System.out.println("\nシトロン\t\t" + citnum + "個");
+		System.out.println("ショコラ\t\t" + chonum + "個");
+		System.out.println("ピスターシュ\t" + pisnum + "個");
+
+		System.out.println("\nをお買い上げですね。\n承りました。");
+
+	}
 
 }
