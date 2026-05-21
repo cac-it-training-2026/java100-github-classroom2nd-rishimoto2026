783be77a76e96e6d23c53d9fa492cd6d6a3b8a66
rishimo <rishimo@C010422941.zipangu.cac.co.jp>
2026-05-21 17:24:14 +0900

    lesson07完了

--- Java100_questions_cac2nd/src/lesson07/challenge11/CastleTown.java
+++ Java100_questions_cac2nd/src/lesson07/challenge11/CastleTown.java
@@ -23,37 +23,126 @@
 
 package lesson07.challenge11;
 
-
 //ここにSamuraiクラスを記述
+class Samurai {
 
+	protected String name;
+
+	void fight() {
+		System.out.println("戦うよ～。");
+	}
+
+	void work() {
+		System.out.println("何かして働くよ～");
+	}
+
+}
 
 //ここにRetainerクラスを記述
+class Retainer extends Samurai {
 
+	/**
+	 * 
+	 */
+	public Retainer() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	protected String domain;
+
+	public Retainer(String name, String domain) {
+		this.name = name;
+		this.domain = domain;
+	}
+
+	void getPaid() {
+		System.out.println("給料をもらうよ～。");
+	}
+
+	void work() {
+		System.out.println("年貢を取り立てるよ～。");
+	}
+
+	public String toString() {
+		return "拙者は" + domain + "藩士、" + name + "ともうす。";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean isMatch = false;
+		Retainer retainer = (Retainer) obj;
+		if (this.domain.equals(retainer.domain)) {
+			isMatch = true;
+		} else {
+			isMatch = false;
+		}
+		return isMatch;
+	}
+}
 
 //ここにRoninクラスを記述
+class Ronin extends Samurai {
 
+	/**
+	 * 
+	 */
+	public Ronin() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/**
+	 * 
+	 */
+	public Ronin(String name) {
+		super();
+		this.name = name;
+	}
+
+	public void work() {
+		System.out.println("傘張るよ～。");
+	}
+
+	@Override
+	public String toString() {
+		return "拙者は武州○△□村の浪人、" + name + "ともうす。";
+	}
+
+}
 
 public class CastleTown {
 
-    public static void main(String[] args) {
-        System.out.println("5人の侍を配列に詰めます。\n");
+	public static void main(String[] args) {
+		System.out.println("5人の侍を配列に詰めます。\n");
 
+		//ここに適切な処理を記述
+		Samurai[] samurais = new Samurai[5];
+		for (int i = 0; i < samurais.length; i++) {
+			int rand = (int) (Math.random() * 10 % 2);
+			if (rand == 0) {
+				samurais[i] = new Retainer();
+			} else {
+				samurais[i] = new Ronin();
+			}
+		}
 
-        //ここに適切な処理を記述
+		System.out.println("詰め終わりました。\n");
+		System.out.println("それぞれの人数を表示してみます。\n");
 
+		int retainerCount = 0;
+		int roninCount = 0;
 
-        System.out.println("詰め終わりました。\n");
-        System.out.println("それぞれの人数を表示してみます。\n");
+		//ここに適切な処理を記述
+		for (int i = 0; i < samurais.length; i++) {
+			if (samurais[i] instanceof Retainer) {
+				retainerCount++;
+			} else {
+				roninCount++;
+			}
+		}
 
-
-        int retainerCount = 0;
-        int roninCount = 0;
-
-
-        //ここに適切な処理を記述
-
-
-        System.out.println("藩士：" + retainerCount + "人");
-        System.out.println("\n浪人：" + roninCount + "人");
-    }
+		System.out.println("藩士：" + retainerCount + "人");
+		System.out.println("\n浪人：" + roninCount + "人");
+	}
 }
--- Java100_questions_cac2nd/src/lesson07/challenge13/CastleTown.java
+++ Java100_questions_cac2nd/src/lesson07/challenge13/CastleTown.java
@@ -21,33 +21,121 @@
 
 package lesson07.challenge13;
 
-
 //ここにICelebrityインターフェースを記述
-
+interface ICelebrity {
+	abstract void learn();
+}
 
 //ここにSamuraiクラスを記述
+class Samurai {
 
+	protected String name;
+
+	void fight() {
+		System.out.println("戦うよ～。");
+	}
+
+	void work() {
+		System.out.println("何かして働くよ～");
+	}
+
+}
 
 //ここにRetainerクラスを記述
+class Retainer extends Samurai implements ICelebrity {
 
+	/**
+	 * 
+	 */
+	public Retainer() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	protected String domain;
+
+	public Retainer(String name, String domain) {
+		this.name = name;
+		this.domain = domain;
+	}
+
+	void getPaid() {
+		System.out.println("給料をもらうよ～。");
+	}
+
+	void work() {
+		System.out.println("年貢を取り立てるよ～。");
+	}
+
+	public String toString() {
+		return "拙者は" + domain + "藩士、" + name + "ともうす。";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean isMatch = false;
+		Retainer retainer = (Retainer) obj;
+		if (this.domain.equals(retainer.domain)) {
+			isMatch = true;
+		} else {
+			isMatch = false;
+		}
+		return isMatch;
+	}
+
+	void learn() {
+		System.out.println("茶道を嗜むよ～。");
+	}
+}
 
 //ここにRoninクラスを記述
+class Ronin extends Samurai {
 
+	/**
+	 * 
+	 */
+	public Ronin() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/**
+	 * 
+	 */
+	public Ronin(String name) {
+		super();
+		this.name = name;
+	}
+
+	public void work() {
+		System.out.println("傘張るよ～。");
+	}
+
+	@Override
+	public String toString() {
+		return "拙者は武州○△□村の浪人、" + name + "ともうす。";
+	}
+
+	void learn() {
+		System.out.println("塾を開くよ～。");
+	}
+
+}
 
 public class CastleTown {
 
-    public static void main(String[] args) {
-        System.out.println("藩士1：");
+	public static void main(String[] args) {
+		System.out.println("藩士1：");
 
+		//ここに適切な処理を記述
+		Retainer retainer = new Retainer();
+		retainer.learn();
 
-        //ここに適切な処理を記述
+		System.out.println("\n浪人1：");
 
+		//ここに適切な処理を記述
+		Ronin ronin = new Ronin();
+		ronin.learn();
 
-        System.out.println("\n浪人1：");
-
-
-        //ここに適切な処理を記述
-
-
-    }
+	}
 }
